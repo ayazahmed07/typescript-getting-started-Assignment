421d288ec7d699bb046ec7fe1b931a6e15cd3b1d
Ayaz ahmed <ayazahmed824@gmail.com>
2024-04-15 06:53:55 +0000

    Question 30 completed

---
+++ 30 Hellow Admin/admin.ts
@@ -0,0 +1,22 @@
+// Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+
+let usernames = [
+  "Ayaz",
+  "Rahan",
+  "Nasir",
+  "Shamim",
+  "Admin",
+  "Hassan",
+  "Junaid",
+];
+
+usernames.forEach((usernames) => {
+  if (usernames === "Admin") {
+    console.log("Hellow Admin, would you like to see a status report");
+  } else {
+    console.log(`Hellow ${usernames}, thank you for loggin in again`);
+  }
+});
