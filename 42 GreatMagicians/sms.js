3e01fc0c820069c11cb16eb345d0d523cf945d50
Ayaz ahmed <ayazahmed824@gmail.com>
2024-05-23 15:58:20 +0000

    modified

--- 42 GreatMagicians/sms.js
+++ 42 GreatMagicians/sms.js
@@ -2,15 +2,18 @@
 // Great Magicians: Start with a copy of your program from Exercise 39.
 //  Write a function called make_great() that modifies the array of magicians by adding the phrase 
 //  the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
-let magicians_names = ["Rock", "Salt", "Pepper"];
-function show_magicians(megicians) {
-    megicians.forEach(megicain => {
-        console.log(magicians_names);
+let magicians = ["Alice", "David", "Chris"];
+function show_magicians(magicians) {
+    magicians.forEach(magician => {
+        console.log(magician);
     });
 }
-function make_great(megicians) {
-    for (let i = 0; i < magicians_names.length; i++) {
-        magicians_names[i] = magicians_names[i] + " the Great";
+show_magicians(magicians);
+function make_great(magicians) {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] + " the Great";
     }
 }
-show_magicians(magicians_names);
+make_great(magicians); // Modifies the original array
+show_magicians(magicians); // Shows modified names
+make_great(magicians);
