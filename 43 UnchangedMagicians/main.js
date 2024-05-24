7c24d038312d9394424dbc8df130a83337a961e0
Ayaz ahmed <ayazahmed824@gmail.com>
2024-05-24 04:55:53 +0000

    updated

---
+++ 43 UnchangedMagicians/main.js
@@ -0,0 +1,24 @@
+"use strict";
+// Unchanged Magicians: Start with your work from Exercise 40. 
+// Call the function make_great() with a copy of the array of magicians’ names. 
+// Because the original array will be unchanged, return the new array and store it in a separate array.
+//  Call show_magicians() with each array to show that you have one array of the
+//   original names and one array with the Great added to each magician’s name.
+Object.defineProperty(exports, "__esModule", { value: true });
+let magicians = ["Rahim", "Rocky", "Pera"];
+function show_magicians(magicians) {
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
+}
+function make_great(magicians) {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] + " the Great";
+    }
+    return magicians;
+}
+let greatmagicians = make_great(magicians.slice()); // creates a new modified array
+console.log("Original magicians:");
+show_magicians(magicians); // Shows Orignal names modified names
+console.log("Great magicians:");
+show_magicians(greatmagicians); // Shows Orignal names modified names
